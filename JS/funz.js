ca9509cfa1fe817477c44bd61c9e4a84d18da519
IMRC21 <marianoradu@mail.com>
2018-04-03 18:35:27 +0200

    Piccole correzioni
    Aggiunte piccole correzzioni ed iniziato lavoro script che esegue dei "salti".

--- JS/funz.js
+++ JS/funz.js
@@ -1,50 +1,76 @@
 ﻿// JavaScript Document
 var es = 0;
 
+//funzione che permette di spostarsi nella pagina in modo che l'header coincida con la grandezza effettiva del div
 function linka(dove){
-	console.log(dove);
-	
 	var altezza = document.getElementById(dove).clientHeight;
 	var head = document.getElementById("header").clientHeight;
-	var he = document.getElementById(dove);	
+	var he = document.getElementById(dove);
 	var ofs = parseInt(he.offsetTop);
-	
+
 	/*
 	console.log("Altezza header:" + head);
 	console.log("Altezza sezione:" + altezza);
 	console.log("offset TOP: " + ofs);
 	*/
-	
+
 	window.scrollTo(0, (ofs-head));
+}
+
+function goto(){
+	/*var h = document.getElementById("header").clientHeight;
+	var he = document.getElementById("bio");
+	var ofs = parseInt(he.offsetTop);
+	window.scrollTo(0, (ofs-h));*/
+	
+	//si rileva se lo scrollo viene effettuato verso il basso o verso l'alto
+	var sE = document.body;
+	sE.addEventListener('wheel', findScrollDirectionOtherBrowsers);
+	
 	
 }
 
+function findScrollDirectionOtherBrowsers(event){
+        	var delta;
+        	if (event.wheelDelta){
+            	delta = event.wheelDelta;
+            }else{
+            	delta = -1 *event.deltaY;
+            }
+        	if (delta < 0){
+            	console.log("giu");
+            }else if (delta > 0){
+            	console.log("su");
+            }
+}
+
+//semplice funzione che mantiene aggioranata la data del copyright
 function cop(){
 	var d = new Date()
 	var anno = d.getFullYear();
-	
+
 	console.log(anno);
-	
+
 	document.getElementById("fsopra").innerHTML = "Copryrights © 4ai " + anno + "  all rights reserved";
-	
+
 }
 /*
 window.onscroll = function() {scrolla()};
 
 
 function scrolla(){
-	
-	
+
+
 	var winScroll = document.getElementsByClassName("articoli").scrollTop;
   	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   	var scrolled = (winScroll / height) * 100;
 	console.log(scrolled);
-	
+
 	document.getElementById("pr-bar").style.width = scrolled + "%";
-	
-	
-	
-	
+
+
+
+
 }*/
 
 function menuDiv() {
@@ -60,18 +86,19 @@ function nascondi(){
 	var div = document.getElementById("mshow");
 	div.style.display = "none";
 			}
-			
+
 function register(){
 	var div = document.getElementById("registerform");
 	document.getElementById("container").style.filter = "blur(3px)";
 	div.style.display = "block";
 			}
-			
+
 function mostra(){
 	var div = document.getElementById("registerform");
 	document.getElementById("container").style.filter = "blur(0px)";
 	div.style.display = "none";
 			}
+
 
 function easteregg(){
 	es += 1;
@@ -80,13 +107,3 @@ function easteregg(){
 		alert("Created By Marian and Tommaso");
 	}
 }
-
-
-
-
-
-
-
-
-
-
